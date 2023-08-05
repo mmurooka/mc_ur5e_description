2e0897139f8d3f10f59ad81524a2375145febb0e
Masaki Murooka <m-murooka@aist.go.jp>
2023-08-05 20:11:07 +0900

    Update convert_from_xacro_to_urdf.sh
    - Refer to ur_description instead of ur_e_description
    - Copy mesh files from the original repository

--- scripts/convert_from_xacro_to_urdf.sh
+++ scripts/convert_from_xacro_to_urdf.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
+MC_UR5E_DESCRIPTION_PATH=`rospack find mc_ur5e_description`
+UR_DESCRIPTION_PATH=`rospack find ur_description`
+
+# Convert xacro to urdf
 TMP_URDF=/tmp/ur5e.urdf
+xacro ${UR_DESCRIPTION_PATH}/urdf/ur5e.xacro > ${TMP_URDF}
 
-source ~/catkin_ws/devel/setup.bash
-
-# Change current directory
-roscd ur_e_description
-
-# Convert from xacro to urdf file
-xacro ./urdf/ur5e_robot.urdf.xacro > ${TMP_URDF}
-
-# Replace package name
-sed -i -e "s/ur_e_description\/meshes\/ur5e/mc_ur5e_description\/meshes/g" ${TMP_URDF}
+# Copy meshes
+cp -r ${UR_DESCRIPTION_PATH}/meshes/ur5e ${MC_UR5E_DESCRIPTION_PATH}/meshes
+sed -i -e "s@ur_description/meshes/ur5e@mc_ur5e_description/meshes@g" ${TMP_URDF}
 
 # Move to project source
-roscd mc_ur5e_description
-if [ ! -d urdf ]; then
-  mkdir urdf
+URDF_DIR=${MC_UR5E_DESCRIPTION_PATH}/urdf
+if [ ! -d ${URDF_DIR} ]; then
+  mkdir -p ${URDF_DIR}
 fi
-mv ${TMP_URDF} urdf/.
+mv ${TMP_URDF} ${URDF_DIR}
